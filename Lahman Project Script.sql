97eb74030a23be4ec246673f8716eac9b8c8078e
Mahi Endatresaw <mahi03@bu.edu>
2025-06-13 13:57:56 -0500

    final push

--- Lahman Project Script.sql
+++ Lahman Project Script.sql
@@ -30,6 +30,7 @@ GROUP BY namefirst||' '||namelast
 ORDER BY total_salary DESC
 LIMIT 1
 --David Price earned the most money in the majors with $81,851,296.00
+
 -- Question 6
 --Find the player who had the most success stealing bases in 2016
 --where __success__ is measured as the percentage of stolen base attempts which are successful
@@ -46,7 +47,69 @@ ORDER BY success_rate DESC
 LIMIT 1;
 --Chris Owings had the most success stealing bases in 2016 (91.30%)--
 
+---Question 9---
+--Which managers have won the TSN Manager of the Year award in both the National League (NL) 
+---and the American League (AL)?
+--Give their full name and the teams that they were managing when they won the award.
 
+--- Question 9 
+WITH nl_winners AS(
+SELECT *
+FROM awardsmanagers
+WHERE awardid = 'TSN Manager of the Year'
+	AND lgid = 'NL'),
 
+al_winners AS(
+SELECT *
+FROM awardsmanagers
+WHERE awardid = 'TSN Manager of the Year'
+	AND lgid ='AL'),
 
+al_and_nl_winners AS(
+SELECT *
+FROM awardsmanagers
+WHERE playerid IN (SELECT playerid
+				   FROM awardsmanagers
+				   WHERE awardid = 'TSN Manager of the Year'
+				   AND lgid ='AL')
+	AND playerid IN (SELECT playerid
+				     FROM awardsmanagers
+					 WHERE awardid = 'TSN Manager of the Year'
+				   	 AND lgid = 'NL')
+	AND awardid = 'TSN Manager of the Year')
+
+SELECT DISTINCT(namefirst), namelast, name, al_and_nl_winners.yearid, al_and_nl_winners.lgid
+FROM al_and_nl_winners
+	LEFT JOIN people USING (playerid)
+	LEFT JOIN managers USING (playerid, yearid)
+	LEFT JOIN teams ON managers.teamid=teams.teamid
+	WHERE teams.yearid>1900
+ORDER BY yearid DESC;
+
+--Question 10--
+--Find all players who hit their career highest number of home runs in 2016
+--Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. 
+--Report the players' first and last names and the number of home runs they hit in 2016.
+WITH player_hr AS (SELECT playerid, yearid, SUM(hr) AS total_hr
+FROM batting
+GROUP BY playerid, yearid),
+career_high AS (SELECT playerid, MAX(total_hr) AS highest_hr
+FROM player_hr
+GROUP BY playerid),
+player_yr AS(SELECT playerid,COUNT(Distinct yearid) AS num_yr
+FROM batting
+GROUP BY playerid),
+hr_2016 AS (SELECT playerid, SUM(hr) AS hr_2016
+FROM batting
+WHERE yearid=2016
+GROUP BY playerid)
+SELECT p.namefirst, p.namelast, h.hr_2016
+FROM hr_2016 AS h
+JOIN career_high AS c
+ON h.playerid=c.playerid AND h.hr_2016=c.highest_hr
+JOIN player_yr AS py
+ON h.playerid=py.playerid AND py.num_yr >=10
+JOIN people AS p
+ON h.playerid=p.playerid
+WHERE h.hr_2016 >0
 
